7c61b2e79e1544f85d4a97d316addc3c4f7ac4df
Eric Ayers <ericzundel@gmail.com>
2025-04-11 09:11:19 -0400

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if (Math.floor(start) != start || Math.floor(end) != end) {
+        return 'ERROR';
+    }
+    console.log (`Type of start is ${typeof (start)}`);
 
+    if (typeof(start) != 'number' || typeof(end) != 'number') {
+        return 'ERROR';
+    }
+    let result = start;
+    if (start < end) {
+        for (let i = start + 1; i <= end; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = start  -1; i >= end; i--) {
+            result += i;
+        } 
+    }
+    return result;
 };
 
 // Do not edit below this line
