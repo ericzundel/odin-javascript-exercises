19a9d61c46413d98be037deb4184f0a618c8c236
Eric Ayers <ericzundel@gmail.com>
2025-04-11 15:50:47 -0400

    Removed archive which fails NPM test, completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let word = input.split("").map((c) => c.toLowerCase()).filter(
+        (c) => (c >= 'a' && c <= 'z') || c === "0" || (c >= "1" && c <= "9")).join("");
+    let reversedWord = word.split("").reverse().join("");
+    return word === reversedWord;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
