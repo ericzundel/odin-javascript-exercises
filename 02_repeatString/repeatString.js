ba997134275c6468dc0bb43ddbb7ca8369bb3f1a
Eric Ayers <ericzundel@gmail.com>
2025-04-10 23:40:25 -0400

    Solved problem 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (val, times = 3) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    let result = ''
+    for (let i = 0; i < times; i++) {
+        result += String(val);
+    }
+    return result;
 };
 
 // Do not edit below this line
