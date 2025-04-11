4d2c56f372645d923629bdba66419327d684f652
Eric Ayers <ericzundel@gmail.com>
2025-04-11 08:59:41 -0400

    Solved 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (val) {
+    let result = '';
+    let length = val.length;
+    for (let i = -1; i >= -length; i--) {
+        result += val.at(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array_val, ...remove_vals) {
+    let result = [];
+    for (let i = 0; i < array_val.length; i++) {
+        let found = false;
+        for (let j = 0; j < remove_vals.length; j++) {
+            if (array_val[i] === remove_vals[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (found == false) {
+            result.push(array_val[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
