d8ce568cf231386d999aba1a203de13102c8a5a5
Eric Ayers <ericzundel@gmail.com>
2025-04-11 15:32:34 -0400

    Completed 08 Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (val1, val2) {
+  return val1 + val2;
 };
 
-const subtract = function() {
-	
+const subtract = function (val1, val2) {
+  return val1 - val2;
 };
 
-const sum = function() {
-	
+const sum = function (array_val) {
+  if (array_val.length == 0) {
+    return 0;
+  }
+  return array_val.reduce((prev, curr) => prev + curr);
 };
 
-const multiply = function() {
-
+const multiply = function (array_val) {
+  return array_val.reduce((total, curr) => total * curr);
 };
 
-const power = function() {
-	
+const power = function (val, exponent) {
+  return val ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (val) {
+  let result = 1;
+  for (let i = 1; i <= val; i++) {
+    result *= i; 
+  }
+  return result;
 };
 
 // Do not edit below this line
